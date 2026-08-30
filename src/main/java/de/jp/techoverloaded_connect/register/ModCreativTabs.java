fb922d89e83b62b7abf643e9e83b5a9b3aae1f8f
jean-piere <jeanpieretraurmann@gmail.com>
2026-08-30 20:28:09 +0200

    fix: Added Creativ Mode Tab Title

--- src/main/java/de/jp/techoverloaded_connect/register/ModCreativTabs.java
+++ src/main/java/de/jp/techoverloaded_connect/register/ModCreativTabs.java
@@ -1,5 +1,6 @@
 package de.jp.techoverloaded_connect.register;
 
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -9,6 +10,7 @@ public class ModCreativTabs {
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MOD_CREATIV_TAB = ModRegister.TABS.register("mod_creativ_tab", ()->
             CreativeModeTab.
                     builder()
+                    .title(Component.translatable("itemGroup.techoverloaded_connect"))
                     .icon(ModItems.LAVA_BOWL::asStack)
                     .build());
     public static void register(IEventBus iEventBus) {
